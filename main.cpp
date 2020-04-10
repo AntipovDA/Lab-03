d4005a14bb48b32844b7f0cef5bac82d282e8870
AntipovDA <dmitrii.antipov.2001@mail.ru>
2020-04-10 13:26:13 +0300

    модуль

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include "histogram.h"
 
 using namespace std;
 
@@ -12,21 +14,7 @@ input_numbers(size_t count) {
     return result;
 }
 
-void
-find_minmax(vector<double> numbers, double& min, double& max) {
-    min = numbers[0];
-    min = numbers[0];
-    max = numbers[0];
-    for (double number : numbers) {
-        if (number < min) {
-            min = number;
-        }
-        if (number > max) {
-            max = number;
-        }
-    }
 
-}
 
 void make_histogram(vector<double> numbers, vector<size_t> &bins, double max, double min, size_t bin_count){
 
